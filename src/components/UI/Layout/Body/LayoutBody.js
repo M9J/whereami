19375d5ed7033f9b875f58c169a880dba8a5367f
M9J <mnsh.exe@gmail.com>
2023-10-07 18:30:34 +0000

    Deploying to build from @ M9J/linearmapp@a5796f59973eb0234162f51f9318cd44ae84524a 🚀

--- src/components/UI/Layout/Body/LayoutBody.js
+++
@@ -1,5 +0,0 @@
-import './LayoutBody.css';
-
-export default function LayoutBody(props) {
-  return <div className="layout-body">{props.children}</div>;
-}
--- src/components/UI/Layout/Footer/LayoutFooter.js
+++
@@ -1,9 +0,0 @@
-import "./LayoutFooter.css";
-
-export default function LayoutFooter() {
-  return (
-    <div className="layout-footer">
-      &copy; 2023 Linearmapp
-    </div>
-  );
-}
