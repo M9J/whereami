156d0b5ab040cf075948ee01a1250bf7a7548e86
Martian Jeirgif <mnsh.exe@gmail.com>
2023-10-08 08:44:38 +0530

    Update TextMap.js
    Additional fields in UI from response

--- src/components/TextMap/TextMap.js
+++ src/components/TextMap/TextMap.js
@@ -1,21 +1,29 @@
 import "./TextMap.css";
 
 export default function TextMap(props) {
-  let locationCity = "";
-  let locationStateCountry = "";
-  if (props?.location?.address) {
-    if (props?.location?.address?.city) {
-      locationCity = `${props.location.address.city}`;
-    }
-    locationStateCountry = `${props.location.address.state}, ${props.location.address.country}`;
-  }
+  let location = props?.location;
+  let address = location?.address;
+  let city = address?.city;
+  let country = address?.country;
+  let displayName = location?.display_name;
+  let neighbourhood = address?.neighbourhood;
+  let postcode = address?.postcode;
+  let state = address?.state;
+  let stateDistrict = address?.state_district;
+  let town = address?.town;
+  let village = address?.village;
   return (
     <div className="text-map">
       <div className="current-location">
-        <div className="current-location-city">{locationCity}</div>
-        <div className="current-location-state-country">
-          {locationStateCountry}
-        </div>
+        Display name: {displayName} <br/><br/>
+        Neighbourhood: {neighbourhood} <br/>
+        Village: {village} <br/>
+        Town: {town} <br/>
+        City: {city} <br/>
+        State district: {stateDistrict} <br/>
+        State: {state} <br/>
+        Country: {country} <br/>
+        Postcode: {postcode} <br/>
       </div>
       <div className="latitude-longitude-details">
         <div className="latitude-text">LAT: {props?.coordinates?.latitude}</div>
