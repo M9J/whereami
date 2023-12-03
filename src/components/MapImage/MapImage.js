8cc5b895c539984e3a564f61e42d125271731ce5
Martian Jeirgif <mgj.exe@gmail.com>
2023-12-03 16:54:49 +0530

    build fixes

--- src/components/MapImage/MapImage.js
+++ src/components/MapImage/MapImage.js
@@ -8,29 +8,29 @@ export default function MapImage(props) {
     "data:image/png;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
   );
 
-  const getRandomImage = async (query = "kerala") => {
-    const randomImageResponse = await fetch(
-      "https://source.unsplash.com/random/?" + query
-    );
-    const randomImage = randomImageResponse?.url;
-    return randomImage;
-  };
+  useEffect(() => {
+    const getRandomImage = async (query = "kerala") => {
+      const randomImageResponse = await fetch(
+        "https://source.unsplash.com/random/?" + query
+      );
+      const randomImage = randomImageResponse?.url;
+      return randomImage;
+    };
 
-  const setInitialImage = async () => {
-    const randomImage = await getRandomImage();
-    if (randomImage) setSrc(randomImage);
-  };
-
-  const loopImage = () => {
-    let t1 = setTimeout(async () => {
-      clearTimeout(t1);
+    const setInitialImage = async () => {
       const randomImage = await getRandomImage();
       if (randomImage) setSrc(randomImage);
-      loopImage();
-    }, 5000);
-  };
+    };
 
-  useEffect(() => {
+    const loopImage = () => {
+      let t1 = setTimeout(async () => {
+        clearTimeout(t1);
+        const randomImage = await getRandomImage();
+        if (randomImage) setSrc(randomImage);
+        loopImage();
+      }, 5000);
+    };
+
     setInitialImage();
     loopImage();
   }, []);
